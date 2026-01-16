4e3636c0e328fc05d2af210a759cebbbd1bf98e2
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2026-01-15 21:06:11 -0300

    Criando o corpo da pagina em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Plus } from 'lucide-react';
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h1>Minha Lista de Tarefas</h1>
+    <div className='container'>
+      <h1>Minha Lista</h1>
+
+      {/* Inputs para adicionar nova tarefa */}
+      <div className='input-group'>
+        <input type="text"  placeholder='Adicione uma nova tarefa'/>
+        <button className='add-btn'>
+          <Plus size={24} />    
+        </button>
+      </div>
+
+      {/* Onde a lista vai aparecer depois */}
+      <div className='tasks-list'>
+        <p className='empty-states'>
+          Você ainda não tem tarefas cadastradas.
+          <br />
+          Crie tarefas e orgazine seus itens a fazer.
+
+        </p>
+
+      </div>
+
     </div>
   )
 }
